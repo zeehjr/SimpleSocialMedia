7709772e12463e5e08c01be4dac7017ab5d0b0d3
José Oliveira <zeehtecnologia@gmail.com>
2020-03-01 02:32:13 -0300

    Backend: Better serializers importing

--- backend/socialmedia/views.py
+++ backend/socialmedia/views.py
@@ -1,8 +1,8 @@
 """ Modules """
 from rest_framework import viewsets
 from django.contrib.auth.models import User, Group
-from .serializers import PostSerializer, CommentSerializer, UserSerializer, GroupSerializer, PostLikeSerializer, CommentLikeSerializer
 from .models import Post, Comment, PostLike, CommentLike
+from . import serializers
 
 # Create your views here.
 
@@ -10,25 +10,25 @@ from .models import Post, Comment, PostLike, CommentLike
 class UserViewSet(viewsets.ModelViewSet):
     """ User ViewSet """
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = serializers.UserSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     """ Group ViewSet """
     queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    serializer_class = serializers.GroupSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     """ Post ViewSet """
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = serializers.PostSerializer
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     """ Comment ViewSet """
     # queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
+    serializer_class = serializers.CommentSerializer
 
     def get_queryset(self):
         return Comment.objects.filter(post=self.kwargs['post_pk'])
@@ -37,10 +37,10 @@ class CommentViewSet(viewsets.ModelViewSet):
 class PostLikeViewSet(viewsets.ModelViewSet):
     """ PostLike ViewSet """
     queryset = PostLike.objects.all()
-    serializer_class = PostLikeSerializer
+    serializer_class = serializers.PostLikeSerializer
 
 
 class CommentLikeViewSet(viewsets.ModelViewSet):
     """ CommentLike ViewSet """
     queryset = CommentLike.objects.all()
-    serializer_class = CommentLikeSerializer
+    serializer_class = serializers.CommentLikeSerializer
